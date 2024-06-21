cd5173bf4533261131e7cc37a36f0471b175d96c
Laercio <96838634+Laercio-Ricardo@users.noreply.github.com>
2024-06-20 21:34:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ function exibirTextoNaTela(tag, texto){
     campo.innerHTML = texto;
 
 }
+//Chama a função exibir Mensagem incial
+exibirMensagemInicial()
 
-exibirTextoNaTela('h1', 'Joguinho do Número Secreto');
-exibirTextoNaTela('p' , 'Tente adivinhar o número secreto entre 1 e 20');
+ //função para exibir a frase incial do  jogo quando o jogo for reiniciado
+
+ function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Joguinho do Número Secreto');
+    exibirTextoNaTela('p' , 'Tente adivinhar o número secreto entre 1 e 20');
+ }
+
 
 //Criando a funcção verificar chute
 function verificarChute(){
@@ -22,7 +29,8 @@ function verificarChute(){
         let palavraTentaviva = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentavivas = `Você descobriu o número secreto com  ${tentativas} ${palavraTentaviva}!`;
         exibirTextoNaTela('p', mensagemTentavivas);
-
+        //aqui ele selcione o elemnto por id
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
         if(chute > numeroSecreto){
             exibirTextoNaTela('p', 'O Número secreto é menor');
@@ -32,9 +40,25 @@ function verificarChute(){
         } 
           //aqui ele some o numero de tentativas pelas vezes que tentou 
           tentativas ++;
+          limparCampo();
+
     }
 
 function gerarNumeroAleatiorio() {
     // math.random cria o numero aleatorio, e o parseInt cria so os inteiros de 1 até 10, e o return antes do parseInt retorna o número aleatorio
     return parseInt(Math.random() *10 + 1);
 }
+//Abaixo a função limpar campo, lá em cima onde tem limpar campo baixo de tentativas é chamada aqui
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+  }
+
+  //função de reiniciar o jogo, pegando o numero reiniciar la do html
+  function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatiorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial()
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+  }
