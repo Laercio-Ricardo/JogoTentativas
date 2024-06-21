39b7a48bdb62119f744e4362ae2e86955350a20e
Laercio <96838634+Laercio-Ricardo@users.noreply.github.com>
2024-06-21 11:19:14 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+//criando a variavel para guardar os numero ja sorteados, os colchetes criam uma lista vazia
+
+let listaDeNumerosSorteados = [];
+// Abaixo estou criando a variavel número secreto e gerando os número aleatoriamente,
+// e no let tentativas ele começa a contar
 let numeroSecreto = gerarNumeroAleatiorio();
-let tentativas = 1
+let tentativas = 1;
 
 //Abaixo tem a função para exibir o html na tela, criando a tag, e após isso a função para exibir o texto na tela
 
@@ -9,7 +14,7 @@ function exibirTextoNaTela(tag, texto){
 
 }
 //Chama a função exibir Mensagem incial
-exibirMensagemInicial()
+exibirMensagemInicial();
 
  //função para exibir a frase incial do  jogo quando o jogo for reiniciado
 
@@ -38,15 +43,32 @@ function verificarChute(){
             exibirTextoNaTela('p', 'O Número secreto é maior');
         }
         } 
-          //aqui ele some o numero de tentativas pelas vezes que tentou 
+          //aqui ele soma o numero de tentativas pelas vezes que tentou 
           tentativas ++;
           limparCampo();
 
     }
 
 function gerarNumeroAleatiorio() {
-    // math.random cria o numero aleatorio, e o parseInt cria so os inteiros de 1 até 10, e o return antes do parseInt retorna o número aleatorio
-    return parseInt(Math.random() *10 + 1);
+    // math.random cria o numero aleatorio, e o parseInt cria so os inteiros de 1 até 10
+    // O let numeroEscolhido cria a variavel, e com o if vamos criar uma condição para o número nao ser escolhido duas vezes
+          let numeroEscolhido = parseInt(Math.random() *10 + 1);
+          //Limitando o número de elementos, o codigo abaixo informa que se a quantidade de elementos for os 10, limpa a lista
+          let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+          if(quantidadeDeElementosNaLista == 10){
+            listaDeNumerosSorteados = [];
+          }
+          //Abaixo o meto inclue o numero escolhido
+          if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+           // Se ja foi escolhido, gera um numero aleatorio
+            return gerarNumeroAleatiorio()
+        
+            }else{// Se nao for escolhido , retorna o numero escolhido, o push serve pra isso
+                listaDeNumerosSorteados.push(numeroEscolhido);
+                console.log(listaDeNumerosSorteados);
+                return numeroEscolhido;
+
+            }
 }
 //Abaixo a função limpar campo, lá em cima onde tem limpar campo baixo de tentativas é chamada aqui
 function limparCampo() {
